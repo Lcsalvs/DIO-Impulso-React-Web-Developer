c01bb7187dfefb09ad9ba3b2ce7f9be36282da1a
Lucas Alves <lcsalves1999@gmail.com>
2021-10-31 20:19:29 -0300

    update file

--- JS - Challenges/Variaveis e Tipos/palindromo.js
+++ JS - Challenges/Variaveis e Tipos/palindromo.js
@@ -1,26 +1,18 @@
-/* ## Atividade
+//Minha solução:
 
-Crie uma função que recebe dois números como parâmetros.
-Confira se os números são iguais.
-Confira se a soma dos números é maior que 10 ou menor que 20.
-Retorne uma string dizendo "Os números num1 e num2 não/são iguais. Sua soma é soma, que é maior/menor que 10 e maior/menor que 20".
-Exemplo:
+// let frase = ["subi no onibus"]
+// let fraseInvertida = frase.reverse()
+// let isPalindrome = frase === fraseInvertida
+// console.log(isPalindrome)
 
-Input: 1, 2
+function checkPalindrome(string) {
+  if(!string) return "String do not exist!";
 
-Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20. */
-
-// Minha solução:
-
-function compNum(num1, num2) {
-  let isEqual = (num1 === num2) ? `Os numeros ${num1} e ${num2} são iguais` : `Os numeros ${num1} e ${num2} não são iguais`;
-  let sum = Number(num1 + num2);
-  let a = (sum > 10) ? "é maior que 10" : "é menor que 10";
-  let b = (sum > 20) ? "é maior que 20" : "é menor que 20";
-  console.log(`${isEqual}. Sua soma é ${sum}, que ${a} e ${b}.`)
+  for(let i = 0; i < string.length; i++) {
+    if(string[i] !== string[string.length - 1 - i]) 
+      return console.log(false);
+  }
+  return console.log(true);
 }
-try { 
-  compNum();
-}catch {
-  console.log("Defina dois números!");
-}
+
+checkPalindrome("gato");
