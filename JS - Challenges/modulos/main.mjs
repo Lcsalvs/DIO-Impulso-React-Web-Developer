624acb9334e7089846eb38d2bad65505b631b9a7
Lucas Alves <lcsalves1999@gmail.com>
2021-11-04 18:16:18 -0300

    adding folder modulos

---
+++ JS - Challenges/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from "./functions.mjs";
+
+console.log(showAge("Lucas", 22));
+console.log(showCity("Lucas", "Vila Velha"));
+console.log(showHobby("Lucas", "jogos"));
